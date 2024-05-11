14cbfd79609ebd99f587a1d3c5409b22a60471e6
rachelhsutzu <120954017+rachelhsutzu@users.noreply.github.com>
2024-05-11 13:05:23 +0800

    Listen & riview fragment2

--- GP_final/GProject/app/src/main/java/com/example/gproject/fragment/WrongFragment.java
+++ GP_final/GProject/app/src/main/java/com/example/gproject/fragment/WrongFragment.java
@@ -3,20 +3,35 @@ package com.example.gproject.fragment;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.gproject.R;
+import com.example.gproject.Review.Fragment_Review_VR;
+import com.example.gproject.Review.Fragment_Review_listening;
+import com.example.gproject.Review.Fragment_Review_reading;
+import com.example.gproject.Review.Fragment_Review_speaking;
+import com.example.gproject.Review.Fragment_Review_writing;
 import com.example.gproject.Review.RReview;
 import com.example.gproject.Review.Review_choose;
-import com.example.gproject.Review.Review_main;
+import com.example.gproject.getDb;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -65,17 +80,47 @@ public class WrongFragment extends Fragment {
         }
     }
 
+    FirebaseAuth auth;
+    private DatabaseReference databaseReference;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootview = inflater.inflate(R.layout.fragment_wrong, container, false);
+
+        ImageButton pencil = rootview.findViewById(R.id.pencil1);
+        Button button = rootview.findViewById(R.id.button);
+        auth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
         Spinner spinner1 = rootview.findViewById(R.id.spinner1);
         ArrayAdapter adapter1 = ArrayAdapter.createFromResource(getContext(),R.array.Review_array, android.R.layout.simple_dropdown_item_1line);
         spinner1.setAdapter(adapter1);
 
-        ImageButton pencil = rootview.findViewById(R.id.pencil1);
-        Button button = rootview.findViewById(R.id.button);
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0: //R
+                        break;
+                    case 1: //L
+                        break;
+                    case 2: //W
+                        getWriting();
+                        break;
+                    case 3: //S
+                        break;
+                    case 4: //VR
+                        break;
+                    default: //R
+                        break;
+                }
+
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
 
         pencil.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -84,12 +129,50 @@ public class WrongFragment extends Fragment {
                 startActivity(intent);
             }
         });
-        button.setOnClickListener(new View.OnClickListener(){
-            public  void onClick(View v){
-                Intent intent = new Intent(getContext(), RReview.class);
-                startActivity(intent);
-            }
-        });
+//        button.setOnClickListener(new View.OnClickListener(){
+//            public  void onClick(View v){
+//                Intent intent = new Intent(getContext(), RReview.class);
+//                startActivity(intent);
+//            }
+//        });
+
     return rootview;
+    }
+
+    @NonNull
+    public void getWriting(){
+        FirebaseUser currentUser = auth.getCurrentUser();
+        if (currentUser != null) {
+            String userId = currentUser.getUid();
+
+            DatabaseReference targetRef = databaseReference
+                    .child("users")
+                    .child(userId)
+                    .child("Writing");
+
+            targetRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.exists()) {
+                        for (DataSnapshot childSnapshot : dataSnapshot.getChildren()) {
+                            String key = childSnapshot.getKey();
+                            // 创建一个按钮
+                            Button button = new Button(getContext());
+                            // 设置按钮的文本为子节点的键名
+                            button.setText(key);
+                        }
+                    } else {
+
+
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+
+            });
+        }
     }
 }
